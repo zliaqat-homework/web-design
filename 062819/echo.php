4f230e54b0b143b3b8c468cd8ef991d6196c78a3
zliaqat-homework <50007757+zliaqat-homework@users.noreply.github.com>
2019-07-01 01:17:37 -0700

    Add files via upload

---
+++ 062819/echo.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    ECHO "Web Design And Development<br>";
+    echo "Web Design And Development<br>";
+    EcHO "Web Design And Development<br>";
+    ?>
+</body>
+</html>
---
+++ 062819/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <h1>My name is Zainab Liaqat</h1>
+    <?php
+    echo "Web Design And Development"
+    ?>
+</body>
+</html>
