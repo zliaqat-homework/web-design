34ac607aa652381b4198769649545aafe7d095df
zliaqat-homework <50007757+zliaqat-homework@users.noreply.github.com>
2019-07-01 10:49:57 -0700

    Add files via upload

---
+++ 010719/add.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+$x = 5  + 15  + 5;
+echo $x;
+?>
+ 
+</body>
+</html>
---
+++ 010719/stat.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <?php
+    function myTest(){
+        static $x = 0;
+        echo $x;
+        $x++;
+    }
+    myTest();
+    echo "<br>";
+    myTest();
+    echo "<br>";
+    myTest();
+    ?>
+</body>
+</html>
